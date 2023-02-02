18d002f4c9847e5ca9b5f85fc5abfd0611d5bf31
evillaz <evillaz123@gmail.com>
2023-02-02 09:53:08 -0500

    correct linters

--- My-Portfolio/script.js
+++ My-Portfolio/script.js
@@ -240,7 +240,7 @@ function validateEmail() {
 
   if (isNotValid) {
     form.setCustomValidity(
-      'You have UPPERCASE in the email you typed, Please use lowercase!'
+      'You have UPPERCASE in the email you typed, Please use lowercase!',
     );
   } else email.setCustomValidity('');
 }
